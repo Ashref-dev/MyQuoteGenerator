0008b79c4458bd52ae7ba722a4344c563c3d7120
Mohamed Ashref Ben Abdallah <109633107+Black-Spear@users.noreply.github.com>
2023-05-24 13:56:41 +0000

    now there's a copy to clipboard button !

--- scripts.js
+++ scripts.js
@@ -11,8 +11,19 @@ let getQuote = () => {
       const author = data.author;
 
       // Displaying the quote and author on the website
-      document.getElementById("quote").innerHTML = quote;
+      document.getElementById("quote").innerHTML = `“${quote}”`;
       document.getElementById("author").innerHTML = "- " + author;
     })
     .catch((error) => console.error(error));
 };
+
+
+const copyContent = async () => {
+  let text = document.getElementById('quote').innerHTML + document.getElementById("author").innerHTML;
+  try {
+    await navigator.clipboard.writeText(text);
+    console.log('Content copied to clipboard');
+  } catch (err) {
+    console.error('Failed to copy: ', err);
+  }
+}
